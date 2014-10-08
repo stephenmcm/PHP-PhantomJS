58464cd689fd4ba5642f08b7173c73138a155d0d
Stephen McMahon <stephen@silverstripe.com.au>
2014-10-08 12:52:52 +1100

    Add getter/setters for variables. Move some message logic into this class.

--- src/HybridLogic/PhantomJS/Runner.php
+++ src/HybridLogic/PhantomJS/Runner.php
@@ -5,9 +5,6 @@ namespace HybridLogic\PhantomJS;
 /**
  * PhantomJS Command Runner
  *
- * $command = new \HybridLogic\PhantomJS\Runner;
- * $result = $command->execute('\www\test.js', 'http://mysite.com', 'another-arg');
- *
  * @package PhantomJS
  **/
 class Runner {
@@ -17,20 +14,30 @@ class Runner {
 	 **/
 	private $bin = '/usr/local/bin/phantomjs';
 
-
 	/**
-	 * @var String currently just allow forced execution
-	 * Probably should be removed
+	 * Truthy/Flasy value to enable exec debug output
+	 * @var Boolean
 	 **/
 	private $debug = null;
+	
+	/**
+	 * Return array from exec
+	 * @var array
+	 */
+	private $result = array();
 
+	/**
+	 * The decoded return from PhantomJS
+	 * @var array
+	 */
+	private $phantomMsg = array();
 
 	/**
 	 *
 	 * @param string Path to phantomjs binary
 	 * @param bool Debug mode
 	 * @return void
-	 * @throws \RuntimeException If bin not found
+	 * @throws \RuntimeException If bin not found, executable or exec is not enabled
 	 */
 	public function __construct($bin = null, $debug = null) {
 		if($bin !== null) $this->bin = $bin;
@@ -47,21 +54,18 @@ class Runner {
 
 		//Exec enabled test
 		$disabled = explode(',', ini_get('disable_functions'));
-		if(in_array('exec', $disabled) && $debug !== 'test'){
-			trigger_error("'exec' appears to be disabled on this server. Pass 'test' in \$debug to run anyway", E_USER_ERROR);
+		if(in_array('exec', $disabled) && !$debug){
+			throw new \RuntimeException("'exec' appears to be disabled on this server. Pass 'test' in \$debug to run "
+										. "anyway");
 		}
 
-	} // end func: __construct
-
-
+	}
 
 	/**
-	 * Execute a given JS file
+	 * Execute PhantomJS with the passed script and arguments
 	 *
-	 * This method should be called with the first argument
-	 * being the JS file you wish to execute. Additional
-	 * PhantomJS command line arguments can be passed
-	 * through as function arguments e.g.:
+	 * This method should be called with the first argument being the JS file you wish to execute. Additional PhantomJS
+	 * command line arguments can be passed through as function arguments e.g.:
 	 *
 	 *     $command->execute('/path/to/my/script.js', 'arg1', 'arg2'[, ...])
 	 *
@@ -74,16 +78,24 @@ class Runner {
 		// Escape
 		$args = func_get_args();
 		$cmd = escapeshellcmd("{$this->bin} " . implode(' ', $args));
-		$cmd .= ' 2>&1';
+		if($this->debug) {
+			$cmd .= ' 2>&1';
+		}
 
 		// Execute
-		exec($cmd, $result);
+		exec($cmd, $this->result);
 
-		$result[] = 'Executed Command:';
-		$result[] = $cmd;
-		return $result;
+		$this->result['executedCommand'] = $cmd;
+		
+		//First entry in the return array is a JSON string of PhantomJS output
+		$this->phantomMsg = json_decode($this->result[0], true);
 
-	} // end func: execute
+		if ($this->phantomMsg['error'] == TRUE) {
+			throw new \RuntimeException('PhantomJS failed with message: ' . $this->phantomMsg['errorMessage']);
+		}
+		
+		return $this->result;
+	}
 
 	/**
 	 * Tests if phantomjs is currently running
@@ -96,11 +108,48 @@ class Runner {
 			echo $escaped_command;
 			exec($escaped_command, $result);
 	        if( count($result) > 2){
+				
 	            return true;
 	        }
-	    }catch(Exception $e){} //Why?
+	    }catch(Exception $e){
+			//exception suppression 
+		}
 
 	    return false;
 	}
 
-} // end class: Runner
+
+	/**
+	 * Returns the JSON return message from PhantomJS converted to an Array
+	 * @return Array
+	 */
+	public function getPhantomMsg() {
+		return $this->phantomMsg;
+	}
+
+	/**
+	 * The raw Array result from exec of PhantomJS
+	 * @return Array
+	 */
+	public function getResult() {
+		return $this->result;
+	}
+	
+	public function getBin() {
+		return $this->bin;
+	}
+
+	public function getDebug() {
+		return $this->debug;
+	}
+
+	public function setBin($bin) {
+		$this->bin = $bin;
+	}
+
+	public function setDebug($debug) {
+		$this->debug = $debug;
+	}
+
+
+}
